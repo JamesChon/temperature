727be4cba37ac60157833bbd51717e9fb2646694
JamesChon <james.chon@sdgku.edu>
2024-01-29 17:23:32 -0800

    complete

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function convertTemperature() {
     // Prompt the user for input and store it in the 'temperature' variable
-    var temperatureInCelsius = parseFloat(prompt("Enter temperature in Celsius:"));
+    let temperatureInCelsius = parseFloat(prompt("Enter temperature in Celsius:"));
 
     // Convert Celsius to Fahrenheit using the formula
-    var temperatureInFahrenheit = (temperatureInCelsius * 9/5) + 32;
+    let temperatureInFahrenheit = (temperatureInCelsius * 9/5) + 32;
 
     // Display the converted temperature
     console.log("Temperature in Fahrenheit: " + temperatureInFahrenheit.toFixed(2));
 
     // Update the result display
-    var result = "The conversion of Celsius " + temperatureInCelsius.toFixed(0) + "°" + " to Fahrenheit is " + temperatureInFahrenheit.toFixed(1) + " °F";
-    document.getElementById("result").innerText =result;
+    let result = "The conversion of Celsius " + temperatureInCelsius.toFixed(0) + "°" + " to Fahrenheit is " + temperatureInFahrenheit.toFixed(1) + " °F";
+    document.getElementById("result").innerText = result;
 }
